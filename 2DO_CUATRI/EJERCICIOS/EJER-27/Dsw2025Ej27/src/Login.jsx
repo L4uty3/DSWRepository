c2c2c5dab05cdff0aabf73fca1a8526a3d95d9d0
L4uty3 <slautaro008@gmail.com>
2025-10-30 18:41:34 -0300

    avances en ejercicio 27

--- 2DO_CUATRI/EJERCICIOS/EJER-27/Dsw2025Ej27/src/Login.jsx
+++ 2DO_CUATRI/EJERCICIOS/EJER-27/Dsw2025Ej27/src/Login.jsx
@@ -3,73 +3,108 @@ import { useForm } from "react-hook-form";
 import Input from "./Input";
 import "./Login.css";
 
-const Login = () => {
+function Login() {
 
-  const [formData, setFormData] = useState({
-    username: '',
-    password: '',
-  });
-
-  const [errors, setErrors] = useState({});
-
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({
-      ...formData,
-      [name]: value,
-    });
+  const onSubmit = (data) => {
+    console.log("Formulario enviado:", data);
   };
+// const Login = () => {
 
-    const validate = () => {
-        const newErrors = {};
-        if (!formData.username) {
-            newErrors.username = "El nombre de usuario es obligatorio";
-        }
-        if (!formData.password) {
-            newErrors.password = "La contraseña es obligatoria";
-        }
-        return newErrors;
-    };
+//   const [formData, setFormData] = useState({
+//     username: '',
+//     password: '',
+//   });
 
-    const handleSubmit = (e) => {
-        e.preventDefault(); //Evitamos que la página se recargue
+//   const [errors, setErrors] = useState({});
+
+//   const handleChange = (e) => {
+//     const { name, value } = e.target;
+//     setFormData({
+//       ...formData,
+//       [name]: value,
+//     });
+//   };
+
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+    // const validate = () => {
+    //     const newErrors = {};
+    //     if (!formData.username) {
+    //         newErrors.username = "El nombre de usuario es obligatorio";
+    //     }
+    //     if (!formData.password) {
+    //         newErrors.password = "La contraseña es obligatoria";
+    //     }
+    //     return newErrors;
+    // };
+
+    // const handleSubmit = (e) => {
+    //     e.preventDefault(); //Evitamos que la página se recargue
         
-        const validationErrors = validate();
-        setErrors(validationErrors);
+    //     const validationErrors = validate();
+    //     setErrors(validationErrors);
 
-        if (Object.keys(validationErrors).length === 0) {
-            console.log("Formulario enviado:", formData);
-        }else{
-            console.log("Errores de validación:", validationErrors);
-        }
-    };
+    //     if (Object.keys(validationErrors).length === 0) {
+    //         console.log("Formulario enviado:", formData);
+    //     }else{
+    //         console.log("Errores de validación:", validationErrors);
+    //     }
+    // };
 
     return (
-    <div className="login-container">
-      <form onSubmit={handleSubmit}>
-        <h1>Iniciar Sesión</h1>
-        <Input
-          label="Usuario"
-          type="text"
-          name="username"
-          value={formData.username}
-          onChange={handleChange}
-          error={errors.username}
-          placeholder="Ingrese su nombre de usuario"
+      <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col gap-2">
+        <input 
+          placeholder="username"
+          {...register('username', {
+            required: 'El nombre de usuario es obligatorio',
+            minLength: { value: 3, message: 'Mínimo 3 caracteres'},
+          })}
         />
-        <Input
-          label="Contraseña"
+        {errors.username && <span>{errors.username.message}</span>}
+
+        <input 
           type="password"
-          name="password"
-          value={formData.password}
-          onChange={handleChange}
-          error={errors.password}
-          placeholder="Ingrese su contraseña"
+          placeholder="password"
+          {...register('password', {
+            required: 'La contraseña es obligatoria',
+            minLength: { value: 6, message: 'Mínimo 6 caracteres'},
+          })}
         />
+        {errors.password && <span>{errors.password.message}</span>}
+
         <button type="submit">Iniciar Sesión</button>
       </form>
-    </div>
-  );
-};
+    );
+}
 
 export default Login;
+
+
+    // <div className="login-container">
+    //   <form onSubmit={handleSubmit}>
+    //     <h1>Iniciar Sesión</h1>
+    //     <Input
+    //       label="Usuario"
+    //       type="text"
+    //       name="username"
+    //       value={formData.username}
+    //       onChange={handleChange}
+    //       error={errors.username}
+    //       placeholder="Ingrese su nombre de usuario"
+    //     />
+    //     <Input
+    //       label="Contraseña"
+    //       type="password"
+    //       name="password"
+    //       value={formData.password}
+    //       onChange={handleChange}
+    //       error={errors.password}
+    //       placeholder="Ingrese su contraseña"
+    //     />
+    //     <button type="submit">Iniciar Sesión</button>
+    //   </form>
+    // </div>
--- 2DO_CUATRI/ejemplos/ejemploReactHook/src/App.jsx
+++ 2DO_CUATRI/ejemplos/ejemploReactHook/src/App.jsx
@@ -5,6 +5,7 @@ function App() {
   const {
     register,
     handleSubmit,
+    formState: { errors },
   } = useForm();
 
   const onSubmit = (data) => {
@@ -18,15 +19,32 @@ function App() {
         placeholder="Nombre"
         {...register('nombre', {
           required: 'El nombre es obligatorio',
-          minlenghth: { value: 3, message: 'Mínimo 3 caracteres'},
+          minLength: { value: 3, message: 'Mínimo 3 caracteres'},
         })}
         />
         {errors.nombre && <span>{errors.nombre.message}</span>}
         
         <input 
-        type="mail"
+        type="email"
         placeholder="Correo"
-        
+        {...register('email', {
+          pattern : {value: /^[^@]+@[^@]+\.[^@ .]{2,}$/,
+            message : 'Correo inválido'
+          },
+        })}
+        />
+        {errors.email && <span>{errors.email.message}</span>}
+
+        <input
+        type="number"
+        placeholder="Edad"
+        {...register('edad', {
+          min: { value: 18, message: 'Debe ser mayor de edad'},
+        })}
+        />
+        {errors.edad && <span>{errors.edad.message}</span>}
+
+        <button type="submit">Enviar</button>
       </form>
     </>
   );
